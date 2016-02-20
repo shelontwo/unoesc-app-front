0d2d91fba5a00c5ffc8525d010690f95cfd614a7
root <root@200-98-115-193.clouduol.com.br>
2016-02-20 10:25:14 -0200

    URL do server alterado

--- app/constants.js
+++ app/constants.js
@@ -1,9 +1,9 @@
 (function() {
-    'use strict';
+   'use strict';
     
     angular
         .module('app')
         .constant('API', {
-            url: 'http://localhost:3000/api/'
+            url: 'http://200.98.115.193:8081/api/'
         });
 })();
